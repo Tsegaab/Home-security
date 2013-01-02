ac464a528d0453b54ebd08c6341c236b27231600
Tekes <tekes@tekes-Satellite-L505D.(none)>
2013-01-02 18:50:19 +0300

    motion detector added

---
+++ python/Motion-detection-OpenCV-master/README.md
@@ -0,0 +1,13 @@
+Motion-detection-OpenCV
+=======================
+
+Python/OpenCV script that detect motion on webcam and allow record it to a file.
+
+
+MotionDetector Features
+-----------------------
+
+* Detect moving object by calculating the average of changing over the hole image.
+* Record to a file the webcam video stream only when something moves.
+
+Note: MotionDetectorContours is an alternative method to detect based on contours and area calculation.
---
+++ python/motion_detector.py
@@ -0,0 +1,35 @@
+#http://www.steinm.com/blog/motion-detection-webcam-python-opencv-differential-images/
+
+import cv2
+
+def diffImg(t0, t1, t2):
+  d1 = cv2.absdiff(t2, t1)
+  d2 = cv2.absdiff(t1, t0)
+  return cv2.bitwise_or(d1, d2)
+
+
+
+cam = cv2.VideoCapture(0)
+s, img = cam.read()
+
+winName = "Movement Indicator"
+cv2.namedWindow(winName, cv2.CV_WINDOW_AUTOSIZE)
+
+# Read three images first:
+t_minus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+t = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+ 
+while s:
+  cv2.imshow( winName, diffImg(t_minus, t, t_plus) )
+  s, img = cam.read() 
+  # Read next image
+  t_minus = t
+  t = t_plus
+  t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+    
+  key = cv2.waitKey(10)
+  if key == 27:
+    cv2.destroyWindow(winName)
+    break 
+print "Goodbye"
